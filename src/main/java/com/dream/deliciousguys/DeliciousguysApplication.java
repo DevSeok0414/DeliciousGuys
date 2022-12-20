bdaa375b0eaa832f82743db5655e575c24ec34d0
SSJ <devseok0414@gmail.com>
2022-12-20 15:46:40 +0900

    project create

---
+++ src/main/java/com/dream/deliciousguys/DeliciousguysApplication.java
@@ -0,0 +1,13 @@
+package com.dream.deliciousguys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliciousguysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliciousguysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dream/deliciousguys/DeliciousguysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dream.deliciousguys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliciousguysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
